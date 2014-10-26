bc4e25b0645346d08d892f65cff9b9d43357e0db
pawnPhilosopher <dvr@fastmail.fm>
2014-10-25 20:37:03 -0400

    commented the robot class

--- scouting486/src/main/java/io/github/positronictraining/scouting486/Robot.java
+++ scouting486/src/main/java/io/github/positronictraining/scouting486/Robot.java
@@ -5,14 +5,15 @@ import java.util.ArrayList;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+@SuppressWarnings("restriction") //suppresses warnings to get rid on nonexistent error messages
 @XmlRootElement
 public class Robot {
 	@XmlElement
-	int teamNumber;
+	int teamNumber; //creates an XML read-and-writable variable called teamNumber for the robot
 	@XmlElement
-	String playerType;
+	String playerType; //creates an XML read-and-writable variable called playerType for the robot
 	@XmlElement
-	ArrayList<Point> pointsScored = new ArrayList<Point>();
+	ArrayList<Point> pointsScored = new ArrayList<Point>(); //creates an XML read-and-writable variable called pointsScored for the robot
 	@XmlElement
-	ArrayList<Point> penaltiesEarned = new ArrayList<Point>();	
+	ArrayList<Point> penaltiesEarned = new ArrayList<Point>(); //creates an XML read-and-writable variable called penalties earned for the robot
 }
