cfb7a64d87beb1c8c97449339e392925629ff3ee
pawnPhilosopher <dvr@fastmail.fm>
2014-10-25 20:57:12 -0400

    commented the file secretary class

--- scouting486/src/main/java/io/github/positronictraining/scouting486/FileSecretary.java
+++ scouting486/src/main/java/io/github/positronictraining/scouting486/FileSecretary.java
@@ -5,8 +5,9 @@ import java.util.ArrayList;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+@SuppressWarnings("restriction") //gets rid of innacurate and annoying error signals
 @XmlRootElement
 public class FileSecretary {
 	@XmlElement
-	public ArrayList<String> fileList = new ArrayList<String>();
+	public ArrayList<String> fileList = new ArrayList<String>(); //makes and XML readable and writable variable called fileList
 }
