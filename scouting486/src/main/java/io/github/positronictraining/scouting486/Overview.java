02267b49d4a52177f3cc4fdcff7e19dec082499c
pawnPhilosopher <dvr@fastmail.fm>
2014-10-25 20:28:52 -0400

    commented overview and added todos

--- scouting486/src/main/java/io/github/positronictraining/scouting486/Overview.java
+++ scouting486/src/main/java/io/github/positronictraining/scouting486/Overview.java
@@ -29,11 +29,14 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.io.File;
 
+//TODO: implement text field and competition button to add data
+//TODO: implement data table to actually display data
+//TODO: implement competition list to display data
+//TODO: implement competition list to change data table on component selection
+
 public class Overview extends JFrame{
 	
-	/**
-	 * 
-	 */
+	//FRAME COMPONENTS
 	private static final long serialVersionUID = 1L;
 	private JTable dataTable;
 	private JTextField competitionNameTxtFld;
@@ -42,41 +45,40 @@ public class Overview extends JFrame{
 	private JComboBox competitionGameComboBox;
 	private JList competitionList;
 	
+	
+	//XML TRANSLATOR INSTANCE
 	private XmlTranslator xmlTranslator = new XmlTranslator();
 	
-	public Overview() {
+	
+	//METHODS
+	public Overview() { //initializes and shows a new overview page whenever a new instance of Overview is made
 		initialize();
 		this.setVisible(true);
 	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		this.setBounds(100, 100, 450, 300);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setPreferredSize(new Dimension(500,400));
-		this.pack();
+	
+	private void initialize() { //Makes a new GUI for the overview
 		
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		this.getContentPane().add(tabbedPane, BorderLayout.CENTER);
+		this.setBounds(100, 100, 500, 400);	//Sets the size of the frame
 		
-		dataTable = new JTable();
-		tabbedPane.addTab("Competition Statistics", null, dataTable, null);
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);	//adds a tabs mechanic to the GUI
+		this.getContentPane().add(tabbedPane, BorderLayout.CENTER); //specifies that it should be front and center
 		
-		competitionList = new JList();
-		tabbedPane.addTab("Competitions List", null, competitionList, null);
+		dataTable = new JTable(); //makes a data-table
+		tabbedPane.addTab("Competition Statistics", null, dataTable, null); //calls that pane "Competition Statistics and adds the data-table to the tabs
 		
-		JPanel panel = new JPanel();
-		tabbedPane.addTab("New Competition", null, panel, null);
-		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0, 0};
-		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		panel.setLayout(gbl_panel);
+		competitionList = new JList(); //makes a competition-list JList
+		tabbedPane.addTab("Competitions List", null, competitionList, null); //calls that pane "Competitions List and adds the JList to the tabs
 		
-		JLabel lblNewCompetition = new JLabel("New Competition");
+		JPanel panel = new JPanel(); //makes a new panel (to eventually put on a new tab)
+		tabbedPane.addTab("New Competition", null, panel, null); //adds that new panel to a new tab called 
+		GridBagLayout gbl_panel = new GridBagLayout(); //makes a grid bag layout for that panel to effectively store all the JComponents
+		gbl_panel.columnWidths = new int[]{0, 0}; //Grid bag stuff
+		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //Grid bag stuff
+		gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE}; //Grid bag stuff
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE}; //Grid bag stuff
+		panel.setLayout(gbl_panel); //adds grid bag stuff to the panel
+		
+		JLabel lblNewCompetition = new JLabel("New Competition");			//New Label called lblNewCompetition
 		lblNewCompetition.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
 		GridBagConstraints gbc_lblNewCompetition = new GridBagConstraints();
 		gbc_lblNewCompetition.anchor = GridBagConstraints.SOUTH;
@@ -84,14 +86,14 @@ public class Overview extends JFrame{
 		gbc_lblNewCompetition.gridx = 0;
 		gbc_lblNewCompetition.gridy = 0;
 		panel.add(lblNewCompetition, gbc_lblNewCompetition);
-		JButton newCompetitionBtn = new JButton("Create New Competition");
-		newCompetitionBtn.addActionListener(new ActionListener() {
+		JButton newCompetitionBtn = new JButton("Create New Competition");	//New JButton called newCompetitionBtn
+		newCompetitionBtn.addActionListener(new ActionListener() {	//What is done if newCompetitionBtn is pressed
 			public void actionPerformed(ActionEvent arg0) {
-				System.out.println("the new competition button was pressed");
+				System.out.println("the new competition button was pressed");	//tells that the button was pressed in the console
 			}
 		});
 		
-		JLabel competitionNameLabel = new JLabel("Competition Name");
+		JLabel competitionNameLabel = new JLabel("Competition Name");	//New Label called lblNewCompetition
 		competitionNameLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 13));
 		GridBagConstraints gbc_competitionNameLabel = new GridBagConstraints();
 		gbc_competitionNameLabel.anchor = GridBagConstraints.WEST;
@@ -100,7 +102,7 @@ public class Overview extends JFrame{
 		gbc_competitionNameLabel.gridy = 2;
 		panel.add(competitionNameLabel, gbc_competitionNameLabel);
 		
-		competitionNameTxtFld = new JTextField();
+		competitionNameTxtFld = new JTextField();					//new TextField called competitionNameTxtFld
 		GridBagConstraints gbc_competitionNameTxtFld = new GridBagConstraints();
 		gbc_competitionNameTxtFld.fill = GridBagConstraints.BOTH;
 		gbc_competitionNameTxtFld.insets = new Insets(0, 0, 5, 0);
@@ -109,7 +111,7 @@ public class Overview extends JFrame{
 		panel.add(competitionNameTxtFld, gbc_competitionNameTxtFld);
 		competitionNameTxtFld.setColumns(10);
 		
-		JLabel lblCompetitionGame = new JLabel("Competition Game");
+		JLabel lblCompetitionGame = new JLabel("Competition Game");	//new label
 		GridBagConstraints gbc_lblCompetitionGame = new GridBagConstraints();
 		gbc_lblCompetitionGame.insets = new Insets(0, 0, 5, 0);
 		gbc_lblCompetitionGame.anchor = GridBagConstraints.WEST;
@@ -117,7 +119,7 @@ public class Overview extends JFrame{
 		gbc_lblCompetitionGame.gridy = 4;
 		panel.add(lblCompetitionGame, gbc_lblCompetitionGame);
 		
-		competitionGameComboBox = new JComboBox();
+		competitionGameComboBox = new JComboBox();					//new combo box
 		GridBagConstraints gbc_competitionGameComboBox = new GridBagConstraints();
 		gbc_competitionGameComboBox.fill = GridBagConstraints.HORIZONTAL;
 		gbc_competitionGameComboBox.insets = new Insets(0, 0, 5, 0);
@@ -125,7 +127,7 @@ public class Overview extends JFrame{
 		gbc_competitionGameComboBox.gridy = 5;
 		panel.add(competitionGameComboBox, gbc_competitionGameComboBox);
 		
-		JLabel lblStartDate = new JLabel("Start Date");
+		JLabel lblStartDate = new JLabel("Start Date");				//new label
 		GridBagConstraints gbc_lblStartDate = new GridBagConstraints();
 		gbc_lblStartDate.insets = new Insets(0, 0, 5, 0);
 		gbc_lblStartDate.anchor = GridBagConstraints.WEST;
@@ -133,7 +135,7 @@ public class Overview extends JFrame{
 		gbc_lblStartDate.gridy = 6;
 		panel.add(lblStartDate, gbc_lblStartDate);
 		
-		startDateTxtFld = new JTextField();
+		startDateTxtFld = new JTextField();							//new text field
 		GridBagConstraints gbc_startDateTxtFld = new GridBagConstraints();
 		gbc_startDateTxtFld.fill = GridBagConstraints.HORIZONTAL;
 		gbc_startDateTxtFld.insets = new Insets(0, 0, 5, 0);
@@ -142,7 +144,7 @@ public class Overview extends JFrame{
 		panel.add(startDateTxtFld, gbc_startDateTxtFld);
 		startDateTxtFld.setColumns(10);
 		
-		JLabel lblEndDate = new JLabel("End Date");
+		JLabel lblEndDate = new JLabel("End Date");					//new label
 		GridBagConstraints gbc_lblEndDate = new GridBagConstraints();
 		gbc_lblEndDate.insets = new Insets(0, 0, 5, 0);
 		gbc_lblEndDate.anchor = GridBagConstraints.WEST;
@@ -150,7 +152,7 @@ public class Overview extends JFrame{
 		gbc_lblEndDate.gridy = 8;
 		panel.add(lblEndDate, gbc_lblEndDate);
 		
-		endDateTxtFld = new JTextField();
+		endDateTxtFld = new JTextField();							//new text field
 		GridBagConstraints gbc_endDateTxtFld = new GridBagConstraints();
 		gbc_endDateTxtFld.fill = GridBagConstraints.HORIZONTAL;
 		gbc_endDateTxtFld.insets = new Insets(0, 0, 5, 0);
@@ -158,7 +160,7 @@ public class Overview extends JFrame{
 		gbc_endDateTxtFld.gridy = 9;
 		panel.add(endDateTxtFld, gbc_endDateTxtFld);
 		
-		endDateTxtFld.setColumns(10);
+		endDateTxtFld.setColumns(10);								//new text field
 		GridBagConstraints gbc_newCompetitionBtn = new GridBagConstraints();
 		gbc_newCompetitionBtn.anchor = GridBagConstraints.EAST;
 		gbc_newCompetitionBtn.gridx = 0;
@@ -166,7 +168,7 @@ public class Overview extends JFrame{
 		panel.add(newCompetitionBtn, gbc_newCompetitionBtn);
 	}
 	
-	public void refreshJList(File file){
-		this.competitionList.setListData(this.xmlTranslator.getCompetitionNames(file));
+	public void refreshJList(File file){							//a method to refresh the JList tab
+		this.competitionList.setListData(this.xmlTranslator.getCompetitionNames(file)); //takes all competitions from a given game and adds it
 	}
 }
