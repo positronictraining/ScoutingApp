3e90c39d5ba8245ef557d321e2b148c20a7c92cf
pawnPhilosopher <dvr@fastmail.fm>
2014-10-25 20:47:45 -0400

    commented point class

--- scouting486/src/main/java/io/github/positronictraining/scouting486/Point.java
+++ scouting486/src/main/java/io/github/positronictraining/scouting486/Point.java
@@ -3,57 +3,46 @@ package io.github.positronictraining.scouting486;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+@SuppressWarnings("restriction") //to get rid of annoying and inaccurate warnings
 @XmlRootElement
 public class Point {
 	
+	//VARIABLES
 	@XmlElement
-	private String pointName;
+	private String pointName;  //creates an XML read-and-writable variable called pointName for the robot
 	@XmlElement
-	private String pointPeriod;
+	private String pointPeriod; //creates an XML read-and-writable variable called pointPeriod for the robot
 	@XmlElement
-	private int pointValue;
+	private int pointValue; //creates an XML read-and-writable variable called pointValue for the robot
 	@XmlElement
-	private int scoringTeam;
+	private int scoringTeam;  //creates an XML read-and-writable variable called scoringTeam for the robot
 	
-	public void setPointType(String pointName, String pointPeriod, int pointValue){
-		this.setPointName(pointName);
-		this.setPointPeriod(pointPeriod);
-		this.setPointValue(pointValue);
+	
+	//METHODS
+	
+	public void setPointType(String pointName, String pointPeriod, int pointValue){ //creates a new point type and stores it's components
+		this.pointName = pointName;
+		this.pointPeriod = pointPeriod;
+		this.pointValue = pointValue;
 	}
 
- 	public void newPointScored(Point pointType, int team){
- 		this.setPointName(pointType.getPointName());
- 		this.setPointPeriod(pointType.getPointPeriod());
- 		this.setPointValue(pointType.getPointValue());
- 		this.setScoringTeam(team);
+ 	public void newPointScored(Point pointType, int team){ //creates a new score of a predetermined point type for a specified team
+ 		this.pointName = pointType.getPointName();
+ 		this.pointPeriod = pointType.getPointPeriod();
+ 		this.pointValue = pointType.getPointValue();
+ 		this.scoringTeam = team;
  	}
  	
- 	private void setPointName(String pointName){
- 		this.pointName = pointName;
- 	}
- 	
- 	private void setPointPeriod(String pointPeriod){
- 		this.pointPeriod = pointPeriod;
- 	}
- 	
- 	private void setPointValue(int pointValue){
- 		this.pointValue = pointValue;
- 	}
- 	
- 	private void setScoringTeam(int scoringTeam){
- 		this.scoringTeam = scoringTeam;
- 	}
-	
-	public String getPointName(){
+	public String getPointName(){ //getter for pointName since pointName is private
 		return this.pointName;
 	}
-	public int getPointValue(){
+	public int getPointValue(){ //getter for pointValue since pointValue is private
 		return this.pointValue;
 	}
-	public int getScoringTeam(){
+	public int getScoringTeam(){ //getter for scoringTeam since scoringTeam is private
 		return this.scoringTeam;
 	}
-	public String getPointPeriod(){
+	public String getPointPeriod(){ //getter for pointPeriod since pointPeriod is private
 		return this.pointPeriod;
 	}
 }
