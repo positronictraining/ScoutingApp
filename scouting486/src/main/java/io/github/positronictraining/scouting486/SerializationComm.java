e71f6546b9547f1cb109758879c9226b695ab150
pawnPhilosopher <dvr@fastmail.fm>
2014-11-06 16:49:56 -0500

    first draft of serialization comm

--- scouting486/src/main/java/io/github/positronictraining/scouting486/SerializationComm.java
+++ scouting486/src/main/java/io/github/positronictraining/scouting486/SerializationComm.java
@@ -1,5 +1,46 @@
 package io.github.positronictraining.scouting486;
 
-public class SerializationComm {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
+
+
+public class SerializationComm {
+	
+	public void writeGame(Game game, String filepath){
+		
+		try{
+			
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("filepath"));
+			objectOutputStream.writeObject(game);
+			System.out.println("game has been told to write to"+filepath);
+
+		} catch(IOException exception){
+
+			exception.printStackTrace();	
+		
+		}
+	
+	}
+	
+	public Game writeGame(String filepath) throws ClassNotFoundException{
+		
+		try{
+			
+			ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("filepath"));
+			Game game = (Game) objectInputStream.readObject();
+			System.out.println("program told to read game at file path"+filepath);
+			return game;
+		
+		} catch(IOException exception){
+			
+			exception.printStackTrace();
+			return null;
+		
+		}
+		
+	}
 }
