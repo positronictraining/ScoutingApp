c4b7ab1bc0ca005cbfc3eb81168ffd58eb449dfe
tmas <tmas@tgaragemedia.com>
2014-11-16 13:02:23 -0500

    Removed extraneous main method

--- scouting486/src/main/java/io/github/positronictraining/scouting486/NewRobotGUI.java
+++ scouting486/src/main/java/io/github/positronictraining/scouting486/NewRobotGUI.java
@@ -20,18 +20,6 @@ private final JTextField textField_1 = new JTextField();
 /**
  * Launch the application.
  */
-public static void main(String[] args) {
-	EventQueue.invokeLater(new Runnable() {
-		public void run() {
-			try {
-				NewRobotGUI frame = new NewRobotGUI();
-				frame.setVisible(true);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	});
-}
 
 protected void setVisible(boolean b) {
 	// TODO Auto-generated method stub
